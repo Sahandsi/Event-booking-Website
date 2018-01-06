897d7f44ce01efeb7f25200a69d36dc85965524e
Sahand <b5038121@my.shu.ac.uk>
2018-01-06 12:18:11 +0000

    added timer to the image slider

--- includes/js/slidingphoto.js
+++ includes/js/slidingphoto.js
@@ -24,3 +24,20 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " w3-opacity-off";
 }
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 6000); // Change image every 6 seconds
+
+    
+}
